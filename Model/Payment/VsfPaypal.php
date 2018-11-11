99b88c578659b363d0fa79aa992b1f7ed5ad2930
Dmitry Schegolihin <d.shegolihin@gmail.com>
2018-11-11 16:46:18 +0400

    Use vsfpaypal

--- Model/Payment/VsfPaypal.php
+++ Model/Payment/VsfPaypal.php
@@ -6,7 +6,7 @@ namespace Develodesign\VsfPaypal\Model\Payment;
 class VsfPaypal extends \Magento\Payment\Model\Method\AbstractMethod
 {
 
-    protected $_code = "vsf-paypal";
+    protected $_code = "vsfpaypal";
     protected $_isOffline = true;
 
     public function isAvailable(
